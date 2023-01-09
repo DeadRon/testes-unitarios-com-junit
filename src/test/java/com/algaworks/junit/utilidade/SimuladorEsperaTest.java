d84c642a1bdcfec4c3c83bb9d56459936ab40590
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-08 23:26:34 -0300

    2.6 Asserções de Timeout

---
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -0,0 +1,26 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimuladorEsperaTest {
+
+    @Test
+    public void deveEsperarEnaoDarTimeout(){
+        assertTimeout(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
+        /*assertTimeout assegura que o método testado deve ser executado em um segundo. Mesmo que o
+        * método execute de forma correta o teste irá invalidar devido ao tempo necessário para executar.
+        * No exemplo leva 10 segundos para executar, o teste falha porque a condição de validação é 1 segundo*/
+    }
+
+    @Test
+    public void deveEsperarEnaoDarTimeoutPreventivamente(){
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(100)));
+        /* assertTimeoutPreemptively faz o mesmo que assertTimeout, porém apresenta falha caso o método não execute no tempo estipulado
+        * diferente do método assertTimeout que iria esperar para validar apenas quando o terminar sua exevução. */
+    }
+
+}
