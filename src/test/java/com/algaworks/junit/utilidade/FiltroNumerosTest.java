9aa208e12901cd9bc83cf67e64c9272f7a926601
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-08 21:58:18 -0300

    2.5. Asserções em listas

---
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -0,0 +1,26 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltroNumerosTest {
+
+    @Test
+    public void deveRetornarNumerosPares(){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        List<Integer> numerosPares = Arrays.asList(2, 4);
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
+        assertIterableEquals(numerosPares, resultadoFiltro, "Comparação de listas de números pares");
+        /*assertIterableEquals verifica CONTEÚDO e ORDEM da lista.
+        Usa o método equals do próprio objeto para fazer a comparação,
+        sem um método Equais implementado, a comparação não funciona*/
+        assertArrayEquals(numerosPares.toArray(new Object[]{}), resultadoFiltro.toArray(new Object[]{}), "Comparação de listas de números pares");
+        /*realiza a mesma ação do assertIterableEquals, mas é preciso converter as listas em arrays*/
+    }
+
+
+}
