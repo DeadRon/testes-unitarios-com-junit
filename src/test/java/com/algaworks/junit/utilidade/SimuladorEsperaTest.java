ed21ead1dd8148aecb359dbf4e9f34080d74b21c
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-11 15:11:41 -0300

    2.10. Ignorando execução dos testes condicionalmente com Assumption

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,7 +1,9 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
@@ -10,8 +12,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimuladorEsperaTest {
 
     @Test
-    @Disabled("não é mais aplicável") //esta anotação faz com o teste não seja executado
+    //@Disabled("Não é mais aplicável") //esta anotação faz com o teste não seja executado
+
+    /* faz com que o teste não execute DADA UMA CONDIÇÃO. Neste caso
+       compara se as variáveis de ambientes possuir o mesmo valor, se sim, o teste executa.
+       OBS: para este teste foi setada a variável(com valor de "PROD" de ambiente na IDE.
+       Este teste não executa por isso.*/
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     public void deveEsperarEnaoDarTimeout(){
+        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")),
+                () -> "ABORTANDO TESTE: NÃO DEVE SER EXECUTADO EM PROD");
+        /*Assumptions.assumeTrue faz com que o teste não execute DADA UMA CONDIÇÃO. Neste caso
+        * compara se as variáveis de ambientes possuir o mesmo valor*/
         assertTimeout(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
         /*assertTimeout assegura que o método testado deve ser executado em um segundo. Mesmo que o
         * método execute de forma correta o teste irá invalidar devido ao tempo necessário para executar.
