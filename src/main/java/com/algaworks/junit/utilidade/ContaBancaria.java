c6fddd51740c32a98ca08818ac708835d8121f9b
Ronaldo Melo <rpm.melkor@gmail.com>
2023-01-12 17:45:43 -0300

    2.14. Desafio - Implementando conta bancária com testes unitários

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -4,24 +4,56 @@ import java.math.BigDecimal;
 
 public class ContaBancaria {
 
+    private BigDecimal saldo;
+
     public ContaBancaria(BigDecimal saldo) {
+
         //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
+        if(saldo == null)
+            throw new IllegalArgumentException("Saldo não pode ser nulo");
+
         //TODO 2 - pode ser zero ou negativo
+        this.saldo = saldo;
     }
 
-    public void saque(BigDecimal valor) {
+    public BigDecimal saque(BigDecimal valor) {
+
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        if(valor == null)
+            throw new IllegalArgumentException("Valor do saque não pode ser nulo");
+        if(valor.compareTo(BigDecimal.ZERO) == 0)
+            throw new IllegalArgumentException("Valor de saque não pode ser de O");
         //TODO 2 - Deve subtrair o valor do saldo
+        int minuendo = saldo.intValue();
+        int subTraendo = valor.intValue();
+        int diferenca = minuendo - subTraendo;
+        saldo = saldo.subtract(valor);
+
         //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+        if(saldo.compareTo(BigDecimal.ZERO) == -1)
+            throw new RuntimeException("Saldo insuficiente para realizar operação de saque");
+
+        return saldo();
     }
 
     public void deposito(BigDecimal valor) {
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        if(valor == null)
+            throw new RuntimeException("Valor para depósito não deve ser nulo");
+
+        if(valor.compareTo(BigDecimal.ZERO) == 0)
+            throw new RuntimeException("Não é possível fazer depósito no valor de 0");
+
+        if(valor.compareTo(BigDecimal.ZERO) < 0)
+            throw new RuntimeException("Não é possível fazer depósito com valores abaixo de 0");
+
         //TODO 2 - Deve adicionar o valor ao saldo
+        saldo = saldo.add(valor);
     }
 
     public BigDecimal saldo() {
+
         //TODO 1 - retornar saldo
-        return null;
+        return saldo;
     }
 }
