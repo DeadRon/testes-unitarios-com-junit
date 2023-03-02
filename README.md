2cc32396eb2b089b5f83206b4924d0bb6ee30886
Ronaldo Melo <ronaldorpm1994@gmail.com>
2023-03-02 02:00:56 -0300

    3.5. Organizando classe de testes com sub-classes e @Nested

--- README.md
+++ README.md
@@ -13,4 +13,10 @@ Given, When, Then -> Dado, Quando, Então -> consiste em uma forma de padronizar
 de testes, nesse caso, consiste em colocar no métodos estas três para dar maior legibilidade sobre
 o que é feito no méotdo
 
+### 3.5. Organizando classe de testes com sub-classes e @Nested
+
+@Nested informa ao Junit que há testes unitários para serem executados na inner class, caso contrário Junit não encontraria. Resulado: 
+
+![alt text for screen readers](/3_5_.png "Text to show on mouseover").
+
 </div>
--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -1,5 +1,6 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,67 +10,101 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ContaBancariaTest {
 
-    @Test
-    public void lancaIllegalArgumentExceptionQuandoSaldoEstiverNulo(){
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null), "Não permite saldo inicial com valor nulo");
-        assertEquals(illegalArgumentException.getMessage(), "Saldo não pode ser nulo");
+    @Nested
+    class Saque {
+
+        @Test
+        public void deveRealizarSaque(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            contaBancaria.saque(BigDecimal.valueOf(300));
+            BigDecimal saldoNaConta = contaBancaria.saldo();
+            assertEquals(BigDecimal.valueOf(700), saldoNaConta, "Valor presente na conta");
+        }
+
+        @Test
+        public void deveLancarIllegalArgumentExceptionQuandoValorSaqueSerNulo(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            IllegalArgumentException valorNulolillegalArgumentException = assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null), "Valida valor para realizar saque!");
+            assertEquals("Valor do saque não pode ser nulo", valorNulolillegalArgumentException.getMessage());
+        }
+
+        @Test
+        public void deveLancarIllegalArgumentExceptionQuandoValorSaqueSerZero(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            IllegalArgumentException valorNulolillegalArgumentException = assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.ZERO), "Valida valor para realizar saque!");
+            assertEquals("Valor de saque não pode ser de O", valorNulolillegalArgumentException.getMessage());
+        }
     }
 
-    @Test
-    public void deveRealizarSaque(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        contaBancaria.saque(BigDecimal.valueOf(300));
-        BigDecimal saldoNaConta = contaBancaria.saldo();
-        assertEquals(BigDecimal.valueOf(700), saldoNaConta, "Valor presente na conta");
+    @Nested
+    class Deposito{
+
+        @Test
+        public void deveRealizarDeposito(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            contaBancaria.deposito(BigDecimal.valueOf(10000));
+            assertEquals(BigDecimal.valueOf(11000), contaBancaria.saldo(), "Novo saldo após depósito");
+        }
+
+        @Test
+        public void deveLancarExceptionQuandoValorDoDepositoSerNulo(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            RuntimeException depositoComNulluntimeException = assertThrows(RuntimeException.class, () -> contaBancaria.deposito(null));
+            assertEquals("Valor para depósito não deve ser nulo", depositoComNulluntimeException.getMessage(), "Não permite valor nulo no depóstio");
+        }
+
+        @Test
+        public void deveLancarExceptionQuandoValorDoDepositoSerZero(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            RuntimeException depositoComZeroRuntimeException = assertThrows(RuntimeException.class, () -> contaBancaria.deposito(BigDecimal.ZERO));
+            assertEquals("Não é possível fazer depósito no valor de 0", depositoComZeroRuntimeException.getMessage(), "Não permite valor zero no depóstio");
+        }
+
+        @Test
+        public void deveLancarExceptionQuandoValorDoDepositoSerMenorQueZero(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            RuntimeException depositoComZeroRuntimeException = assertThrows(RuntimeException.class, () -> contaBancaria.deposito(BigDecimal.valueOf(-4500)));
+            assertEquals("Não é possível fazer depósito com valores abaixo de 0", depositoComZeroRuntimeException.getMessage(), "Não permite valor menor que zero no depóstio");
+        }
     }
 
-    @Test
-    public void deveLancarIllegalArgumentExceptionQuandoValorSaqueSerNulo(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        IllegalArgumentException valorNulolillegalArgumentException = assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null), "Valida valor para realizar saque!");
-        assertEquals("Valor do saque não pode ser nulo", valorNulolillegalArgumentException.getMessage());
-    }
+    @Nested
+    class Saldo {
 
-    @Test
-    public void deveLancarIllegalArgumentExceptionQuandoValorSaqueSerZero(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        IllegalArgumentException valorNulolillegalArgumentException = assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.ZERO), "Valida valor para realizar saque!");
-        assertEquals("Valor de saque não pode ser de O", valorNulolillegalArgumentException.getMessage());
-    }
+        @Test
+        public void lancaIllegalArgumentExceptionQuandoSaldoEstiverNulo(){
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null), "Não permite saldo inicial com valor nulo");
+            assertEquals(illegalArgumentException.getMessage(), "Saldo não pode ser nulo");
+        }
 
-    @Test
-    public void deveLancarRuntimeExceptionQuandoSaldoEstiverComValorZero(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> contaBancaria.saque(BigDecimal.valueOf(1100)), "Valida saldo restante após realizar saque");
-        assertEquals("Saldo insuficiente para realizar operação de saque", runtimeException.getMessage());
-    }
+        @Test
+        public void deveRealizarSaque(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            contaBancaria.saque(BigDecimal.valueOf(300));
+            BigDecimal saldoNaConta = contaBancaria.saldo();
+            assertEquals(BigDecimal.valueOf(700), saldoNaConta, "Valor presente na conta");
+        }
 
-    @Test
-    public void deveRealizarDeposito(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        contaBancaria.deposito(BigDecimal.valueOf(10000));
-        assertEquals(BigDecimal.valueOf(11000), contaBancaria.saldo(), "Novo saldo após depósito");
-    }
+        @Test
+        public void deveLancarIllegalArgumentExceptionQuandoValorSaqueSerNulo(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            IllegalArgumentException valorNulolillegalArgumentException = assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null), "Valida valor para realizar saque!");
+            assertEquals("Valor do saque não pode ser nulo", valorNulolillegalArgumentException.getMessage());
+        }
 
-    @Test
-    public void deveLancarExceptionQuandoValorDoDepositoSerNulo(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        RuntimeException depositoComNulluntimeException = assertThrows(RuntimeException.class, () -> contaBancaria.deposito(null));
-        assertEquals("Valor para depósito não deve ser nulo", depositoComNulluntimeException.getMessage(), "Não permite valor nulo no depóstio");
-    }
+        @Test
+        public void deveLancarIllegalArgumentExceptionQuandoValorSaqueSerZero(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            IllegalArgumentException valorNulolillegalArgumentException = assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.ZERO), "Valida valor para realizar saque!");
+            assertEquals("Valor de saque não pode ser de O", valorNulolillegalArgumentException.getMessage());
+        }
 
-    @Test
-    public void deveLancarExceptionQuandoValorDoDepositoSerZero(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        RuntimeException depositoComZeroRuntimeException = assertThrows(RuntimeException.class, () -> contaBancaria.deposito(BigDecimal.ZERO));
-        assertEquals("Não é possível fazer depósito no valor de 0", depositoComZeroRuntimeException.getMessage(), "Não permite valor zero no depóstio");
-    }
+        @Test
+        public void deveLancarRuntimeExceptionQuandoSaldoEstiverComValorZero(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
+            RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> contaBancaria.saque(BigDecimal.valueOf(1100)), "Valida saldo restante após realizar saque");
+            assertEquals("Saldo insuficiente para realizar operação de saque", runtimeException.getMessage());
+        }
 
-    @Test
-    public void deveLancarExceptionQuandoValorDoDepositoSerMenorQueZero(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(1000));
-        RuntimeException depositoComZeroRuntimeException = assertThrows(RuntimeException.class, () -> contaBancaria.deposito(BigDecimal.valueOf(-4500)));
-        assertEquals("Não é possível fazer depósito com valores abaixo de 0", depositoComZeroRuntimeException.getMessage(), "Não permite valor menor que zero no depóstio");
     }
-
 }
